31307ae42fbd596ce85354d2dcb1429400fbb09e
Meindert <meindertduin@hotmail.com>
2020-11-05 13:31:58 +0100

    added vuetify and some styling loading pasge

--- ClientApp/src/views/Home.vue
+++ ClientApp/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-img 
+            src="https://picsum.photos/350/165?random"
+            min-height="500"
+    >
+      <v-row class="fill-height ma-6" justify="center" align-content="center">
+        <div class="text-center">
+          <div class="display-2 font-weight-bold white--text">Welkom bij mijn</div>
+          <div class="display-3 font-weight-bold white--text">Portofolio</div>
+          <v-btn class="ma-5" fab dark large>
+            <v-icon dark>
+              mdi-chevron-double-down
+            </v-icon>
+          </v-btn>
+        </div>
+      </v-row>
+    </v-img>
+    <v-row>
+      <v-btn>Hello wolrd</v-btn>
+    </v-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  @Component({
+    name: 'Home',
+  })
+  export default class Home extends Vue{
+  
   }
-}
 </script>
+
