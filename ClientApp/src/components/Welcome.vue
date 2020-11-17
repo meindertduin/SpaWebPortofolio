b7e22485be728ff56def35cc18969f434bac573c
Meindert <meindertduin@hotmail.com>
2020-11-17 20:52:38 +0100

    welcome text styling

--- ClientApp/src/components/Welcome.vue
+++ ClientApp/src/components/Welcome.vue
@@ -2,10 +2,10 @@
     <v-img src="files/misty-waters.jpg" min-height="900px" >
         <v-row class="fill-height ma-6" justify="center" align-content="center">
             <div class="text-center">
-                <div class="display-2 font-weight-bold white--text">Welkom bij mijn</div>
-                <div class="display-3 font-weight-bold white--text">Web-Portofolio</div>
-                <v-btn class="ma-5" fab dark large @click="scrollToProfile">
-                    <v-icon dark>
+                <div class="welcome-text">Web-Portfolio</div>
+                <div class="welcome-text" id="name-text">Meindert van Duin</div>
+                <v-btn icon class="ma-5" fab dark x-large @click="scrollToProfile">
+                    <v-icon dark x-large> 
                         mdi-chevron-double-down
                     </v-icon>
                 </v-btn>
@@ -35,5 +35,34 @@
 </script>
 
 <style scoped>
+    .welcome-text{
+        font-family: 'Teko', sans-serif;
+        color: #65d0db;
+        grid-column: 1;
+        grid-row: 1;
+        align-self: end;
+        justify-self: center;
 
+        margin: 0;
+        z-index: 1;
+        font-size: 2rem;
+        text-transform: uppercase;
+        animation: glow 6s ease-in-out infinite alternate;
+        text-align: center;
+    }
+
+    #name-text{
+        font-size: 4rem;
+        font-weight: bolder;
+        margin-bottom: 4rem;
+    }
+    
+    @keyframes glow {
+        from {
+            text-shadow: 0 0 20px #2d9da9;
+        }
+        to {
+            text-shadow: 0 0 30px #34b3c1, 0 0 10px #4dbbc7;
+        }
+    }
 </style>
