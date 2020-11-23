02a858df3a927b0b6fca9fd25a3082333d04e199
Meindert <meindertduin@hotmail.com>
2020-11-23 20:23:26 +0100

    fixed typo

--- ClientApp/src/config/oidc.ts
+++ ClientApp/src/config/oidc.ts
@@ -4,7 +4,7 @@ export const oidcSettings:VuexOidcClientSettings = {
     authority: `${process.env.VUE_APP_API_URL}`,
     client_secret: process.env.VUE_APP_CLIENT_SECRET,
     clientId: 'SpaPortofolioWebClient',
-    redirectUri: 'https://localhost:5001/oidc-callback',
+    redirectUri: `${process.env.VUE_APP_API_URL}/oidc-callback`,
     responseType: 'code',
     scope: 'openid profile IdentityServerApi Role',
     postLogoutRedirectUri: `${process.env.VUE_APP_API_URL}`,
