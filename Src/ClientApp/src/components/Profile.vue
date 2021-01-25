0a59a5862e9c9a44c02dd92e77cbb8e4c490d8f4
Meindert <meindertduin@hotmail.com>
2021-01-25 11:56:16 +0100

    changed profile tet

--- Src/ClientApp/src/components/Profile.vue
+++ Src/ClientApp/src/components/Profile.vue
@@ -9,15 +9,14 @@
                     <div class="display-1 font-weight-bold mt-5">Ikke in 't kort</div>
                     <div style="margin-top: 2em; margin-bottom: 1em">
                         <div class="ma-2">
-                            Ik ben Meindert, 21 jaar en programmeer al een tijdje met veel plezier websites en andere projecten/
-                            In de afgelopen maanden heb ik al een aantal projecten uitgevoerd waarbij ik vanaf het begin tot aan het hosten aan toe alle touwtjes in handen mee had.
-                            Hiermee heb ik dan ook zeker veel ervaring mee opgedaan. Ik voel me vrij zeker over mijn vaardigheden en daarom ben ik dus opzoek naar een baan waamee
-                            ik mezelf nog verder kan ontwikkelen. Natuurlijk wil ik doorgroeien in dit vakgebied! 
+                          Ik ben Meindert, 21 jaar en programmeer al een tijdje met veel plezier websites en andere projecten. Zo heb ik al bij een aantal projecten weten creëren waarbij ik van begin tot eind alle touwtjes in handen had. Op deze manier heb ik ervaring opgedaan in de frontend, backend en ook bij het hosten van applicaties. Ik voel me vrij zeker over mijn vaardigheden en ben daarom opzoek naar een baan waarmee ik mijzelf verder kan ontwikkelen. Natuurlijk wil ik doorgroeien in dit vakgebied!
                         </div>
                         <div class="ma-2">
-                            Ik ben dus opzoek naar een baan waarmee ik mijn
-                            voetdruk in de wereld van proffesioneel fullstack development kan zetten.
-                        </div>                        
+                          Mijn interesses liggen vooral bij de backend, maar ben ook zeker niet vies van werken aan de frontend van een applicatie en hoop een mooi balans te vinden tussen de twee.
+                        </div>      
+                        <div class="ma-2">
+                          Ik ben dus opzoek naar een baan waarmee ik mijn voetafdruk in de wereld van software ontwikkeling kan zetten.
+                        </div>
                     </div>
                 </div>
             </v-col>
